1e94d449fac964b24cfc2223e496c66bcc81fbb6
Sean <sean.yasno@gmail.com>
2018-08-25 21:37:34 +0300

    First Commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset;
+	private bool isRotating;
+	private bool rotateRight;
+	private Vector3 newRotatePos;
+	private float rotateSpeed = 1;
+	private float rotationTime;
+	
+	private void Start() {
+		offset = transform.position - player.transform.position;
+	}
+
+	private void Update() {
+		if (Input.GetKeyDown(KeyCode.E) && !isRotating)
+			StartCoroutine(RotateMe(Vector3.up * -90f, .5f));
+
+		if (Input.GetKeyDown(KeyCode.Q) && !isRotating)
+			StartCoroutine(RotateMe(Vector3.up * 90f, .5f));
+	}
+
+	private IEnumerator RotateMe(Vector3 byAngles, float inTime) {
+		isRotating = true;
+		var fromAngle = transform.rotation;
+		var toAngle = Quaternion.Euler(transform.eulerAngles + byAngles);
+		for (var t = 0f; t < 1; t += Time.deltaTime/inTime) {
+			transform.rotation = Quaternion.Slerp(fromAngle, toAngle, t);
+			yield return null;
+			transform.rotation = toAngle;
+		}
+		isRotating = false;
+		player.GetComponent<PlayerController>().CameraRotated();
+	}
+
+	private void LateUpdate() {
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed = 4f;
+
+	private Vector3 forward, right;
+
+	private void Start() {
+		forward = Camera.main.transform.forward;
+		forward.y = 0;
+		forward = Vector3.Normalize(forward);
+		right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
+	}
+
+	private void Update() {
+		if (Input.anyKey)
+			Move();
+	}
+
+	public void CameraRotated() {
+		forward = Camera.main.transform.forward;
+		forward.y = 0;
+		forward = Vector3.Normalize(forward);
+		right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
+	}
+
+	private void Move() {
+		//Vector3 direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+		Vector3 rightMovement = right * moveSpeed * Time.deltaTime * Input.GetAxisRaw("Horizontal");
+		Vector3 upMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxisRaw("Vertical");
+
+		Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
+
+		if (heading != Vector3.zero)
+			transform.forward = heading;	
+		transform.position += rightMovement;
+		transform.position += upMovement;
+	}
+}
