e18642d728e67b91d34f79b05515638c06e2eb2e
Laguland <s.skubis1@unimail.derby.ac.uk>
2018-09-01 00:51:12 +0200

    menu done

---
+++ Assets/Scripts/UI/MenuManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject Credits;
+
+    // Play game
+    public void Play()
+    {
+        SceneManager.LoadScene("level");
+    }
+
+    // Show/Hide Credits
+    public void ShowHideCredits()
+    {
+        if(Credits.active)
+        {
+            Credits.SetActive(false);
+        }
+        else
+        {
+            Credits.SetActive(true);
+        }
+    }
+
+    // quit game
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
