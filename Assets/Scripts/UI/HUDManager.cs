b5fe851d2d4785f72f6d163b368ae1dd2e321846
Laguland <s.skubis1@unimail.derby.ac.uk>
2018-08-31 22:15:19 +0200

    player's HUD added

---
+++ Assets/Scripts/UI/HUDManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDManager : MonoBehaviour {
+
+    // get labels
+    public Text label_EnCount;
+    public Text label_LampCount;
+
+    void Update()
+    {
+        GameObject[] aMonster = GameObject.FindGameObjectsWithTag("Enemy");
+        GameObject[] aLamp = GameObject.FindGameObjectsWithTag("Lamp");
+
+        label_EnCount.text = aMonster.Length.ToString();
+        label_LampCount.text = aLamp.Length.ToString();
+    }
+}
